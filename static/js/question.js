fb8bc50346e5cb32c7f2b7fe12f73446f9a89dae
LyndseyR <lyndseyroka@Lyndseys-MacBook-Pro.local>
2017-04-09 14:29:19 -0400

    updated links for mobile and desktop navigation

--- static/js/question.js
+++ static/js/question.js
@@ -2,26 +2,26 @@ $(document).ready(function(){
   var time
 
   window.onload = myTimer();
-  
-  function myTimer(){
-  var timer = 11;
-  time = setInterval(function () {
-    timer--;
-    if(timer > 4) {
-      $('.timer').html(timer + ' seconds');
-    }
-    else if (timer > 0) {
-      $('.timer').html('hurry ' + timer + ' seconds');
-    }
-    else {
-      window.location = "end.html";
-    }
-  }, 1000);
 
-}
-$('.test').click(function(){
-  clearInterval(time);
-  myTimer();
-});
+  function myTimer(){
+    var timer = 11;
+    time = setInterval(function () {
+      timer--;
+      if(timer > 4) {
+        $('.timer').html(timer + ' seconds');
+      }
+      else if (timer > 0) {
+        $('.timer').html('hurry ' + timer + ' seconds');
+      }
+      else {
+        window.location = "end.html";
+      }
+    }, 1000);
+  }
+
+  $('.test').click(function(){
+    clearInterval(time);
+    myTimer();
+  });
 
 });
